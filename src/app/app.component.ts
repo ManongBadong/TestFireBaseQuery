84da103e451fd17c7d1d6c6456366c70e1df8f69
ManongBadong <ray_reyes91@yahoo.com>
2022-05-21 15:45:05 +0800

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,31 +5,38 @@ import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit  {
-
-  formGroup!: FormGroup
+export class AppComponent implements OnInit {
+  formGroup!: FormGroup;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.formGroup = new FormGroup({
       title: new FormControl(null),
-      content: new FormControl(null)
+      content: new FormControl(null),
     });
   }
 
   onCreatePost(postData: { title: string; content: string }) {
-    this.http.post("https://badongrecipe-default-rtdb.asia-southeast1.firebasedatabase.app/post.json", postData).subscribe(
-      (responseData => {
+    this.http
+      .post(
+        'https://badongrecipe-default-rtdb.asia-southeast1.firebasedatabase.app/post.json',
+        postData
+      )
+      .subscribe((responseData) => {
         console.log(responseData);
-      })
-    );
+      });
   }
 
-  test12() {
-    
+  private fetchPosts() {
+    this.http
+      .get(
+        'https://badongrecipe-default-rtdb.asia-southeast1.firebasedatabase.app/post.json'
+      )
+      .subscribe((posts) => {
+        console.log(posts);
+      });
   }
-
 }
