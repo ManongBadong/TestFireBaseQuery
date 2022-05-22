44252c0f8b3d26fd31b2a082c2fe47fed2bdf23c
ManongBadong <ray_reyes91@yahoo.com>
2022-05-22 12:51:20 +0800

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptorService } from './service/auth-interceptor.service';
+import { LoggingInterceptorService } from './service/logging-interceptor.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule ],
   declarations: [ AppComponent, HelloComponent ],
-  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptorService, multi: true}],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/service/auth-interceptor.service.ts
+++ src/app/service/auth-interceptor.service.ts
@@ -2,15 +2,12 @@ import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http"
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    console.log('Request is on the way.')
-    // you can modify requests here.
-    console.log(req);
+    console.log('First!');
     const modifiedReq = req.clone({
       headers: req.headers.append('Manong', 'Badong')
     });
-    console.log(modifiedReq);
     return next.handle(modifiedReq);
 
-    // you can also use something to modify the response but better by careful. using .pipe .map . tap
+    // you can also use something to modify the response but better by careful. using .pipe .map . tapsdf
   }
 }
