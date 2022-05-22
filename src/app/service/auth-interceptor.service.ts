980bf5aa705e2d470b075bb42c8006ada8e689ff
ManongBadong <ray_reyes91@yahoo.com>
2022-05-22 11:27:22 +0800

    1 file modified

--- src/app/service/auth-interceptor.service.ts
+++ src/app/service/auth-interceptor.service.ts
@@ -4,9 +4,11 @@ export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     console.log('Request is on the way.')
     // you can modify requests here.
+    console.log(req);
     const modifiedReq = req.clone({
       headers: req.headers.append('Manong', 'Badong')
     });
+    console.log(modifiedReq);
     return next.handle(modifiedReq);
   }
 }
